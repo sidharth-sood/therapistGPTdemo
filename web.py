f7d7b61aa3f8a61baf50cdf3c4351726d94e3bfb
Sidharth Sood <108302940+sidharth-sood@users.noreply.github.com>
2023-07-17 13:33:39 -0500

    Update web.py

--- web.py
+++ web.py
@@ -1,10 +1,32 @@
 import gradio as gr
+import openai, config, subprocess
 
-def greet(name):
-    return "Hello " + name + "!"
+messages = [{"role": "system", "content": '//engineer prompt for a therapy session'}]
 
-demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+def transcribe(audio):
+    global messages
 
-demo.launch()
+    audio_filename_with_extension = audio + '.wav'
+    os.rename(audio, audio_filename_with_extension)
+    
+    audio_file = open(audio_filename_with_extension, "rb")
+    transcript = openai.Audio.transcribe("whisper-1", audio_file)
 
-# Get cursor back
+    messages.append({"role": "user", "content": transcript["text"]})
+
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+
+    system_message = response["choices"][0]["message"]
+    messages.append(system_message)
+
+    subprocess.call(["say", system_message['content']])
+
+    chat_transcript = ""
+    for message in messages:
+        if message['role'] != 'system':
+            chat_transcript += message['role'] + ": " + message['content'] + "\n\n"
+
+    return chat_transcript
+
+ui = gr.Interface(fn=transcribe, inputs=gr.Audio(source="microphone", type="filepath"), outputs="text").launch()
+ui.launch()
